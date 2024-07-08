63747d4161d8a86bb92e9803229e020e97d0a0ba
hyeon-b <20201009@sungshin.ac.kr>
2024-07-08 20:00:11 +0900

    fix: 경로 에러 해결

--- word2pdf.py
+++ word2pdf.py
@@ -1,12 +1,12 @@
 from docx2pdf import convert
 import os
+import pythoncom
 
 # docx파일을 pdf 파일로 변환하는 함수
 def word2pdf(upload_path, result_path, file_name):
-    print(upload_path, file_name)
-    print(result_path)
-    print("--------------------------")
-    docx_path = os.path.join(upload_path, file_name)
-    pdf_path = os.path.join(result_path, file_name)
+    pythoncom.CoInitialize()
+
+    docx_path = os.path.abspath(upload_path)
+    pdf_path = os.path.abspath(os.path.join(result_path, file_name)+'.pdf')
 
     return convert(docx_path, pdf_path)
