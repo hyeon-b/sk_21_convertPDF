9a7e51b18fff7832cd25786170c70caa7892d3c8
hyeon-b <20201009@sungshin.ac.kr>
2024-07-08 16:51:05 +0900

    xlsx.py 변수명 수정

--- xlsx2pdf.py
+++ xlsx2pdf.py
@@ -5,11 +5,12 @@ def xlsx2pdf(upload_path, result_path, file_name):
     #Create a workbook
     workbook = Workbook()
     #Load an Excel file
-    workbook.LoadFromFile(file_name)
+    workbook.LoadFromFile(upload_path)
 
     #Iterate through the worksheets in the file
     for sheet in workbook.Worksheets:
         FileName =  sheet.Name + ".pdf"
         #Save each sheet to a separate PDF
-        sheet.SaveToPdf(FileName)
+        result_path = os.path.join(result_path, f'{FileName}.pdf')
+        sheet.SaveToPdf(result_path)
     workbook.Dispose()
