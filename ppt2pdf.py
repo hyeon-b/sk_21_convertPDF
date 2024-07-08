f6ba5b8b073b908e3de14e90ba36fc85ed4622e0
hyeon-b <20201009@sungshin.ac.kr>
2024-07-08 18:54:28 +0900

    fix:경로 수정

--- ppt2pdf.py
+++ ppt2pdf.py
@@ -4,13 +4,13 @@ from os.path import abspath
 import pythoncom
 
 def ppt2pdf(upload_path, result_path, file_name): 
-    IN_PATH = upload_path 
-    OUT_PATH = os.path.join(result_path, file_name)
+    IN_PATH = os.path.abspath(upload_path)
+    OUT_PATH = os.path.abspath(os.path.join(result_path, file_name))
 
     #파워포인트 열기
     powerpoint = win32com.client.Dispatch("Powerpoint.Application", pythoncom.CoInitialize()) 
 
-    ppt_file = powerpoint.Presentations.Open(upload_path)
+    ppt_file = powerpoint.Presentations.Open(IN_PATH)
     ppt_file.SaveAs(f'{OUT_PATH}.pdf', 32)   #저장할 폴더에 .pdf 확장자로 저장(32: pdf파일을 의미하는 formatType)
     ppt_file.Close()
 
