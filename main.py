309fd86903ce8d9eb5c56d0d63db865638f2b1dd
hyeon-b <20201009@sungshin.ac.kr>
2024-07-09 09:38:25 +0900

    slack api main에 연결

--- main.py
+++ main.py
@@ -12,7 +12,9 @@ from pdf_translator import translate_pdf
 
 UPLOAD_DIR = 'uploads'
 RESULT_DIR = 'converts'
-
+# Slack API 토큰과 메시지를 보낼 채널 설정
+SLACK_API_TOKEN = "your-slack-api-token"
+SLACK_CHANNEL = "your-channel-id"
 # 객체 생성
 app = Flask(__name__)
 
@@ -44,7 +46,7 @@ def convert():
     if is_slack:
         message =''' pdf converter를 이용해주셔서 감사합니다.
         변환된 pdf파일입니다. '''
-        send_slack(upload_path, message)
+        send_slack(SLACK_API_TOKEN, SLACK_CHANNEL, upload_path, message)
     if is_trans:
         translated_file = translate_pdf(RESULT_DIR, only_file_name)
         return send_file(translated_file, as_attachment=True)
